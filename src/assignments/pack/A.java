e65611007d59c1c01f0510545e5d042780a5755c
rajdeeppoddar15@gmail.comemail <you@example.com>
2021-09-25 02:17:56 +0530

    initial commit

---
+++ src/assignments/pack/A.java
@@ -0,0 +1,14 @@
+package assignments.pack;
+
+public class A {
+	public static void main(String args[]) {
+		B obj1 = new B();
+		obj1.add(16, 0);
+		obj1.add(20.90, 30.00);
+		obj1.add(20, 30, 12);
+		
+	}
+	
+	
+
+}
